801af3d6e5332429da1a5b074dcbbdd19ee3bf4f
javiercmelendrez <javier.c.melendrez@gmail.com>
2022-09-08 13:22:43 -0400

    add services

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,6 +1,23 @@
 package service;
 
+import model.Customer;
+
+import java.util.Collection;
+
 public class CustomerService {
-    public void addCustomer();
+    public void addCustomer(String email, String firstName, String lastName)
+    {
+
+    }
+
+    public Customer getCustomer(String customerEmail)
+    {
+
+    }
+
+    public Collection<Customer> getAllCustomers(){
+
+    }
+
 
 }
---
+++ src/service/ReservationService.java
@@ -0,0 +1,36 @@
+package service;
+
+import model.Customer;
+import model.IRoomRepository;
+import model.Reservation;
+import model.Room;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class ReservationService {
+    public void addRoom(IRoomRepository room){
+
+    }
+
+    public Room getARoom(String roomId){
+
+    }
+
+    public Reservation reserveARoom(Customer customer, Room room, Date checkInDate, Date checkOutDate){
+
+    }
+
+    public Collection<Room> findRooms(Date checkInDate, Date checkOutDate){
+
+    }
+
+    public Collection<Reservation> getCustomersReservation(Customer customer){
+
+    }
+
+    public void printAllReservation(){}
+
+
+
+}
